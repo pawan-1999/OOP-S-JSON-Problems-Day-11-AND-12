2e64a0275523bf1f425cc52642cb7fefaddaebd4
pawan-1999 <guptapawan767@gmail.com>
2021-08-08 23:43:18 +0530

    Added ; Main Method To Call And Print Output

---
+++ Problem-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_1
+{
+    class Program
+    {
+        public static List<Inventory> inventories = new List<Inventory>();
+
+        static void Main(string[] args)
+        {
+            FileReader fileReader = new FileReader();
+            var inventories = fileReader.GetInventry();
+
+            foreach (var i in inventories)
+            {
+                Console.WriteLine("Name : " + i.Name);
+                Console.WriteLine("Weight : " + i.Weight);
+                Console.WriteLine("Price : " + i.Price);
+                Console.WriteLine("##########################");
+            }
+        }
+    }
+}
