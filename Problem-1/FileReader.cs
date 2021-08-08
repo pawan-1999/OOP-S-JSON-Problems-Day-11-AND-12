f817e925e773c6c4d39f6e6d3ff15b70d128fa8c
pawan-1999 <guptapawan767@gmail.com>
2021-08-08 23:42:19 +0530

    Added : Reading Json File

---
+++ Problem-1/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Problem_1
+{
+    public class FileReader
+    {
+        public static List<Inventory> inventories = new List<Inventory>();
+
+        public List<Inventory> GetInventry()
+        {
+            string text = File.ReadAllText(@"C:\Users\gupta\OneDrive\Desktop\DataOFPulses.json");
+            inventories = JsonConvert.DeserializeObject<List<Inventory>>(text);
+            return inventories;
+        }
+
+    }
+}
