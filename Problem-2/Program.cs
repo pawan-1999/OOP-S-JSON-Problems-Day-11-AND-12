3596e05acddbc61d20110a4b63ffee7fb35b6fd3
pawan-1999 <guptapawan767@gmail.com>
2021-08-08 23:45:14 +0530

    Added : Main Method

---
+++ Problem-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_2
+{
+    class Program
+    {
+        public static List<Inventory> inventories = new List<Inventory>();
+
+        static void Main(string[] args)
+        {
+            FileReader fileReader = new FileReader();
+            var inventories = fileReader.GetInventry();
+
+            foreach (var i in inventories)
+            {
+                Console.WriteLine("Name : " + i.Name);
+                Console.WriteLine("Weight : " + i.Weight);
+                Console.WriteLine("Price : " + i.Price);
+                Console.WriteLine("#######################");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Total price of pulses are :");
+            Console.WriteLine();
+
+            foreach (var i in inventories)
+            {
+                Console.WriteLine("Name: " + i.Name);
+                Console.WriteLine("Total Price: " + i.Weight * i.Price);
+                Console.WriteLine("************************");
+            }
+        }
+    }
+}
